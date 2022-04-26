fd67446e17eb1afed49830d5a9af1e43a020c4dd
Juan Antonio González <jaglezrivera@gmail.com>
2022-04-26 09:50:51 +0200

    supplier

---
+++ src/main/java/es/fpdual/supplier/SupplierMain.java
@@ -0,0 +1,12 @@
+package es.fpdual.supplier;
+
+import java.util.function.Supplier;
+
+public class SupplierMain {
+
+    public static void main(String[] args) {
+        Supplier<String> i = () -> "Juan";
+
+        System.out.println("Value is: " + i.get());
+    }
+}
